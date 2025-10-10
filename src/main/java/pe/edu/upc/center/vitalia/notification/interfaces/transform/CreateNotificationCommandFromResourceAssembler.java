f42edb92f4ec0baac8a6733479cd5af4e42d265a
Miguel26112001 <u202220294@upc.edu.pe>
2025-10-09 19:40:49 -0500

    feat: added notification assemblers

---
+++ src/main/java/pe/edu/upc/center/vitalia/notification/interfaces/transform/CreateNotificationCommandFromResourceAssembler.java
@@ -0,0 +1,5 @@
+package pe.edu.upc.center.vitalia.notification.interfaces.transform;
+
+public class CreateNotificationCommandFromResourceAssembler {
+    // Assembler for creating notification commands from resources
+}
---
+++ src/main/java/pe/edu/upc/center/vitalia/notification/interfaces/transform/NotificationResourceFromEntityAssembler.java
@@ -0,0 +1,5 @@
+package pe.edu.upc.center.vitalia.notification.interfaces.transform;
+
+public class NotificationResourceFromEntityAssembler {
+    // Assembler for transforming notification entities to resources
+}
